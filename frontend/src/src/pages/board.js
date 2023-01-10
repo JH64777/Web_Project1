ff0b61669433c135025fbd5fdb8c7b4817609b6c
JH64777 <65661921+JH64777@users.noreply.github.com>
2023-01-10 06:29:03 -0800

    Add files via upload

---
+++ frontend/src/src/pages/board.js
@@ -0,0 +1,12 @@
+function Board(){
+    return (
+        <div>
+            <header>
+                <h1>Board</h1>
+                <hr />
+            </header>
+        </div>
+    );
+}
+
+export default Board;
