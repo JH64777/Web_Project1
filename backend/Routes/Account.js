21cd5ddbca6f3b50e07ffbf5375b6cb7629cc6fc
JH64777 <65661921+JH64777@users.noreply.github.com>
2023-01-18 04:14:02 +0900

    New Version
    I added sending code to email and check code is right.

--- backend/Routes/Account.js
+++ backend/Routes/Account.js
@@ -2,12 +2,12 @@ const express = require('express');
 const router = express.Router();
 const account = require('../API/AccountAPI.js');
 const Hashing = require('../API/encryt.js');
-// const cors = require('cors');
+const mailer = require('../API/Mail.js');
 
 router.post('/', (req, res) => {
     Hashing.Hash(req.body['password'])
     .then((result) => { // Making command
-        const command = `INSERT INTO user VALUES(\'${req.body['id']}\', \'${result.value}\', \'${req.body['nickname']}\', \'${result.key}\');`;
+        const command = `INSERT INTO user VALUES(\'${req.body['id']}\', \'${result.value}\', \'${req.body['nickname']}\', \'${result.key}\', \'${req.body['mail']}\');`;
         return command;
     })
     .then((command) => { // Insert Data
@@ -29,6 +29,11 @@ router.post('/CheckNick', function(req, res){ // The Checking NickName
     account.Check(command)
     .then(resolve => res.send(resolve))
     .catch(rejects => console.log(rejects));
+});
+
+router.post('/SendMail', function(req, res){ // Send Code to Client's Mail
+    mailer.MailSender(req.body['mail'])
+    .then((resolve) => res.send(resolve));
 });
 
 module.exports = router;
