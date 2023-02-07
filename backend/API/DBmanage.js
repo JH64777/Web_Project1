d54916a6484b76046e6d96be0d5838960dcb034c
JH64777 <65661921+JH64777@users.noreply.github.com>
2023-02-08 01:08:20 +0900

    I added DB manager prototype version!
    I will(want to) use this for only DB manage API

---
+++ backend/API/DBmanage.js
@@ -0,0 +1,15 @@
+const model = require('../DB/DBmanagement/models'); // You need to have models in your Disk
+
+function Insert(table, value) { // You need to match column names in DB and value
+    return model[table].create(value);
+}
+
+function Select(table, value){ // You need to match column names in DB and value
+    return model[table].findAll({
+        where : {
+            value
+        }
+    })
+} // ex> Select * from user where user_nickname = "example" => table = 'user', value = { user_nick : "example" }
+
+module.exports = {Insert : Insert, Select : Select};
