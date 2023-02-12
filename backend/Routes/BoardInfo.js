61dfe8f1e8dd68de6ecc20012ba2ef6c6293fe4d
JH64777 <65661921+JH64777@users.noreply.github.com>
2023-02-12 21:57:51 +0900

    I advanced posting function!
    The beta version of posting function can't combine few expression in contents (like ", ', //, # etc )
    But! now is available!

--- backend/Routes/BoardInfo.js
+++ backend/Routes/BoardInfo.js
@@ -1,16 +1,17 @@
 const express = require("express");
 const router = express.Router();
-const DB = require('../API/AccountAPI.js');
+const DB = require('../API/DBmanage.js');
 
 router.post('/', (req, res) => {
     res.send({"response" : "Hello"});
 });
-
+// `Insert into post(writer,title,time,codenum,contents) VALUES(\"${req.body["name"]}\",\"${req.body["title"]}\",\"${req.body["time"]}\",\"${req.body["num"]}\",\"${req.body["contents"]}\");`;
 router.post('/Posting', (req,res) => {// when data contain " or etc then occur error so we have to encoding it and when data is shown monitor we need to decode that
-    const command = `Insert into post(writer,title,time,codenum,contents) VALUES(\"${req.body["name"]}\",\"${req.body["title"]}\",\"${req.body["time"]}\",\"${req.body["num"]}\",\"${req.body["contents"]}\");`;
-    DB.Insert(command)
-    .then(result => res.send(result))
-    .catch(err => res.send(err));
-});
-
+    DB.Insert('post',req.body)
+    .then(result => res.send({ "response" : "Well Done!" }))
+    .catch(err => {
+        console.log(err);
+        res.send({ "response" : "Check Length of your contents!" });
+        });
+    });
 module.exports = router;
